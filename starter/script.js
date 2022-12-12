81b3ed8c806c648d606db1cee40a2f1e6f6de569
damnsemicolon <shannoninengland@gmail.com>
2022-12-12 00:22:36 +0000

    added clear fields button because it makes sense

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,18 @@
 
 $(document).ready(function() {
 
+//      moment.js code to show date & time
 $("#currentDay").text("Date: " + moment().format('dddd, Do MMMM yyyy'));
 $("#currentTime").text("Time: " + moment().format('hh:mm:ss A'));
+//      Set up current time for referencing
+var currentTime = moment().format("HH");
 
-
+//      Clear local storage and show default in scheduler
+$("#clearFieldsBtn").click(function (event) {
+    event.preventDefault;
+    $("textarea").val("");
+    localStorage.clear();
+  });
 
 });
 
